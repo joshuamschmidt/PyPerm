3b81a689fbef62561b4c575645143371439fd0d8
joshuamschmidt <joshmschmidt1@gmail.com>
2021-03-18 09:37:49 +1030

    example analysis

--- example_analysis.py
+++ example_analysis.py
@@ -1,2 +1,20 @@
 import pysetperm as psp
 
+# used for all sub analyses
+features = psp.Features('data/genes.txt', 2000)
+annotations = psp.AnnotationSet('data/kegg.txt', features.features_user_def, 5)
+n_perms = 50000
+cores = 4
+# specific inputs
+e_input = psp.Input('data/eastern_candidates.txt',
+                    'data/eastern_background.txt.gz',
+                    features.features_user_def,
+                    annotations.annotation_array)
+
+# permutations
+e_permutations = psp.Permutation(e_input.background_features, e_input.n_candidates, n_perms, cores)
+
+e_per_set = psp.SetPerPerm(e_permutations.permutations,
+                           annotations.annotation_array,
+                           cores,
+                           e_input.n_candidate_per_function)
