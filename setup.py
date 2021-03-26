831ad2722f90c7b076b563f2131d1345e6c99af0
joshuamschmidt <joshmschmidt1@gmail.com>
2021-03-26 12:18:17 +1030

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PySetPerm',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='joshua schmidt',
+    author_email='joshmschmidt1@gmail.com',
+    description='genomic set tests'
+)
