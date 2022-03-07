5ea4c48b7b2af6b6950218b8dc28535a78b08dbc
joshuamschmidt <joshmschmidt1@gmail.com>
2022-03-07 11:22:16 +1030

    chaning project structure

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='PySetPerm',
+    name='set_perm',
     version='0.0.1',
     description='genomic set tests',
     py_modules=['pysetperm'],
     package_dir={'': 'src'},
-    url='',
-    license='',
+    url='https://github.com/joshuamschmidt/set_perm',
+    license='MIT',
     author='joshua schmidt',
     author_email='joshmschmidt1@gmail.com'
 )
