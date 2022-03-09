c75f413fe81302aa9b8c869b6dcf0ec7fa9b3039
joshuamschmidt <joshmschmidt1@gmail.com>
2022-03-09 16:30:10 +1030

    cli works

---
+++ tests.sh
@@ -0,0 +1,149 @@
+#--- kegg
+
+bin/test_tuple_parse \
+--candidates "central,data/central-0.000192-candidate.snps.bed.gz" "eastern,data/eastern-0.000228-candidate.snps.bed.gz" "ancestral,data/ancestral-0.005-candidate.snps.bed.gz" \
+--background "central,data/pbsnj-bg.snps.bed.gz" "eastern,data/pbsnj-bg.snps.bed.gz" "ancestral,data/ancestral-bg.bed.gz" \
+--feature_def data/genes.txt \
+--function_def data/kegg.txt \
+--min_set_size 10 \
+--n_perms 10000 \
+--prefix "match-ancestral_0.5%" \
+--joint "central,eastern" "central,ancestral" "eastern,ancestral" "central,eastern,ancestral" \
+--gene_def 2000 \
+--threads 10
+
+bin/test_tuple_parse \
+--candidates "central,data/central-4e-05-candidate.snps.bed.gz" "eastern,data/eastern-3.5e-05-candidate.snps.bed.gz" "ancestral,data/ancestral-0.001-candidate.snps.bed.gz" \
+--background "central,data/pbsnj-bg.snps.bed.gz" "eastern,data/pbsnj-bg.snps.bed.gz" "ancestral,data/ancestral-bg.bed.gz" \
+--feature_def data/genes.txt \
+--function_def data/kegg.txt \
+--min_set_size 10 \
+--n_perms 10000 \
+--prefix "match-ancestral_0.1%" \
+--joint "central,eastern" "central,ancestral" "eastern,ancestral" "central,eastern,ancestral" \
+--gene_def 2000 \
+--threads 10
+
+bin/test_tuple_parse \
+--candidates "central,data/central-2e-05-candidate.snps.bed.gz" "eastern,data/eastern-1.5e-05-candidate.snps.bed.gz" "ancestral,data/ancestral-0.0005-candidate.snps.bed.gz" \
+--background "central,data/pbsnj-bg.snps.bed.gz" "eastern,data/pbsnj-bg.snps.bed.gz" "ancestral,data/ancestral-bg.bed.gz" \
+--feature_def data/genes.txt \
+--function_def data/kegg.txt \
+--min_set_size 10 \
+--n_perms 10000 \
+--prefix "match-ancestral_0.05%" \
+--joint "central,eastern" "central,ancestral" "eastern,ancestral" "central,eastern,ancestral" \
+--gene_def 2000 \
+--threads 10
+
+#--- vip
+bin/test_tuple_parse \
+--candidates "central,data/central-0.000192-candidate.snps.bed.gz" "eastern,data/eastern-0.000228-candidate.snps.bed.gz" "ancestral,data/ancestral-0.005-candidate.snps.bed.gz" \
+--background "central,data/pbsnj-bg.snps.bed.gz" "eastern,data/pbsnj-bg.snps.bed.gz" "ancestral,data/ancestral-bg.bed.gz" \
+--feature_def data/genes.txt \
+--function_def data/vip.txt \
+--min_set_size 10 \
+--n_perms 10000 \
+--prefix "match-ancestral_0.5%" \
+--joint "central,eastern" "central,ancestral" "eastern,ancestral" "central,eastern,ancestral" \
+--gene_def 2000 \
+--threads 10
+
+bin/test_tuple_parse \
+--candidates "central,data/central-4e-05-candidate.snps.bed.gz" "eastern,data/eastern-3.5e-05-candidate.snps.bed.gz" "ancestral,data/ancestral-0.001-candidate.snps.bed.gz" \
+--background "central,data/pbsnj-bg.snps.bed.gz" "eastern,data/pbsnj-bg.snps.bed.gz" "ancestral,data/ancestral-bg.bed.gz" \
+--feature_def data/genes.txt \
+--function_def data/vip.txt \
+--min_set_size 10 \
+--n_perms 10000 \
+--prefix "match-ancestral_0.1%" \
+--joint "central,eastern" "central,ancestral" "eastern,ancestral" "central,eastern,ancestral" \
+--gene_def 2000 \
+--threads 10
+
+bin/test_tuple_parse \
+--candidates "central,data/central-2e-05-candidate.snps.bed.gz" "eastern,data/eastern-1.5e-05-candidate.snps.bed.gz" "ancestral,data/ancestral-0.0005-candidate.snps.bed.gz" \
+--background "central,data/pbsnj-bg.snps.bed.gz" "eastern,data/pbsnj-bg.snps.bed.gz" "ancestral,data/ancestral-bg.bed.gz" \
+--feature_def data/genes.txt \
+--function_def data/vip.txt \
+--min_set_size 10 \
+--n_perms 10000 \
+--prefix "match-ancestral_0.05%" \
+--joint "central,eastern" "central,ancestral" "eastern,ancestral" "central,eastern,ancestral" \
+--gene_def 2000 \
+--threads 10
+
+#--- SIV
+
+bin/test_tuple_parse \
+--candidates "central,data/central-0.000192-candidate.snps.bed.gz" "eastern,data/eastern-0.000228-candidate.snps.bed.gz" "ancestral,data/ancestral-0.005-candidate.snps.bed.gz" \
+--background "central,data/pbsnj-bg.snps.bed.gz" "eastern,data/pbsnj-bg.snps.bed.gz" "ancestral,data/ancestral-bg.bed.gz" \
+--feature_def data/genes.txt \
+--function_def data/siv.txt \
+--min_set_size 10 \
+--n_perms 10000 \
+--prefix "match-ancestral_0.5%" \
+--joint "central,eastern" "central,ancestral" "eastern,ancestral" "central,eastern,ancestral" \
+--gene_def 2000 \
+--threads 10
+
+bin/test_tuple_parse \
+--candidates "central,data/central-4e-05-candidate.snps.bed.gz" "eastern,data/eastern-3.5e-05-candidate.snps.bed.gz" "ancestral,data/ancestral-0.001-candidate.snps.bed.gz" \
+--background "central,data/pbsnj-bg.snps.bed.gz" "eastern,data/pbsnj-bg.snps.bed.gz" "ancestral,data/ancestral-bg.bed.gz" \
+--feature_def data/genes.txt \
+--function_def data/siv.txt \
+--min_set_size 10 \
+--n_perms 10000 \
+--prefix "match-ancestral_0.1%" \
+--joint "central,eastern" "central,ancestral" "eastern,ancestral" "central,eastern,ancestral" \
+--gene_def 2000 \
+--threads 10
+
+bin/test_tuple_parse \
+--candidates "central,data/central-2e-05-candidate.snps.bed.gz" "eastern,data/eastern-1.5e-05-candidate.snps.bed.gz" "ancestral,data/ancestral-0.0005-candidate.snps.bed.gz" \
+--background "central,data/pbsnj-bg.snps.bed.gz" "eastern,data/pbsnj-bg.snps.bed.gz" "ancestral,data/ancestral-bg.bed.gz" \
+--feature_def data/genes.txt \
+--function_def data/siv.txt \
+--min_set_size 10 \
+--n_perms 10000 \
+--prefix "match-ancestral_0.05%" \
+--joint "central,eastern" "central,ancestral" "eastern,ancestral" "central,eastern,ancestral" \
+--gene_def 2000 \
+--threads 10
+
+#--- SIV modules
+bin/test_tuple_parse \
+--candidates "central,data/central-0.000192-candidate.snps.bed.gz" "eastern,data/eastern-0.000228-candidate.snps.bed.gz" "ancestral,data/ancestral-0.005-candidate.snps.bed.gz" \
+--background "central,data/pbsnj-bg.snps.bed.gz" "eastern,data/pbsnj-bg.snps.bed.gz" "ancestral,data/ancestral-bg.bed.gz" \
+--feature_def data/genes.txt \
+--function_def data/siv_modules.txt \
+--min_set_size 10 \
+--n_perms 10000 \
+--prefix "match-ancestral_0.5%" \
+--joint "central,eastern" "central,ancestral" "eastern,ancestral" "central,eastern,ancestral" \
+--gene_def 2000 \
+--threads 10
+
+bin/test_tuple_parse \
+--candidates "central,data/central-4e-05-candidate.snps.bed.gz" "eastern,data/eastern-3.5e-05-candidate.snps.bed.gz" "ancestral,data/ancestral-0.001-candidate.snps.bed.gz" \
+--background "central,data/pbsnj-bg.snps.bed.gz" "eastern,data/pbsnj-bg.snps.bed.gz" "ancestral,data/ancestral-bg.bed.gz" \
+--feature_def data/genes.txt \
+--function_def data/siv_modules.txt \
+--min_set_size 10 \
+--n_perms 10000 \
+--prefix "match-ancestral_0.1%" \
+--joint "central,eastern" "central,ancestral" "eastern,ancestral" "central,eastern,ancestral" \
+--gene_def 2000 \
+--threads 10
+
+bin/test_tuple_parse \
+--candidates "central,data/central-2e-05-candidate.snps.bed.gz" "eastern,data/eastern-1.5e-05-candidate.snps.bed.gz" "ancestral,data/ancestral-0.0005-candidate.snps.bed.gz" \
+--background "central,data/pbsnj-bg.snps.bed.gz" "eastern,data/pbsnj-bg.snps.bed.gz" "ancestral,data/ancestral-bg.bed.gz" \
+--feature_def data/genes.txt \
+--function_def data/siv_modules.txt \
+--min_set_size 10 \
+--n_perms 10000 \
+--prefix "match-ancestral_0.05%" \
+--joint "central,eastern" "central,ancestral" "eastern,ancestral" "central,eastern,ancestral" \
+--gene_def 2000 \
+--threads 10
